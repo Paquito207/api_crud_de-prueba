4273a957195508fc226ae3ff9ffd38b3a0758c56
Paquito207 <robertalexiscachayneme@gmail.com>
2025-02-24 16:19:54 -0500

    Delete API_CRUD_DOIS directory

--- API_CRUD_DOIS/models/pedido_productos.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type PedidoProductos struct {
-	PedidoId   *Pedidos   `orm:"column(pedido_id);rel(fk)"`
-	ProductoId *Productos `orm:"column(producto_id);rel(fk)"`
-	Cantidad   int        `orm:"column(cantidad)"`
-}
